6c90b5d75a5e0d3983c0fc01a92e9eefb2b6d5c4
Deepak M S <souparnikad40@gmail.com>
2022-08-21 12:59:35 +0530

    user added to database

--- auth.py
+++ auth.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from pydantic import  BaseModel
-from  typing import Optional
-import models
+from typing import Optional
+from sqlalchemy.orm import  Session
+from database import  SessionLocal, engine
+from fastapi import FastAPI , Depends
 from passlib.context import CryptContext
+from pydantic import BaseModel
+
+import models
+
+
 class CreateUser(BaseModel):
     username: str
     email: Optional[str]
@@ -10,24 +15,37 @@ class CreateUser(BaseModel):
     last_name: str
     password: str
 
+
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield  db
+    finally:
+        db.close()
+
 def get_password_hash(password):
-    return  bcrypt_context.hash(password)
+    return bcrypt_context.hash(password)
+
 
 # create new user -post
 @app.post('/create/user')
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db : Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
-    create_user_model.first_name=create_user.first_name
-    create_user_model.last_name=create_user.last_name
-    create_user_model.username=create_user.username
+    create_user_model.first_name = create_user.first_name
+    create_user_model.last_name = create_user.last_name
+    create_user_model.username = create_user.username
     hash_password = get_password_hash(create_user.password)
-    create_user_model.hashed_password=hash_password
-    create_user_model.is_active=True
+    create_user_model.hashed_password = hash_password
+    create_user_model.is_active = True
+
+    db.add(create_user_model)
+    db.commit()
 
     return create_user_model
-
