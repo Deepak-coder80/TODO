167ad2aab377f4be933913c58b1bdf8bc4f5827b
Deepak M S <souparnikad40@gmail.com>
2022-08-21 09:45:52 +0530

    keys seperated

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import keys
 
 # create database url
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{keys.username}:{keys.password}@localhost/{keys.database}"
 
 # create engine
 engine = create_engine(
